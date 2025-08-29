4d6ed68ad0f5d861c97b65f43b85db6dd9b8b2e5
SahanaAvantika <sahanaavantika@gmail.com>
2025-08-29 18:58:25 +0800

    Update frontend

--- frontend/lynx.config.ts
+++ frontend/lynx.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from '@lynx-js/rspeedy'
 
 import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
 import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+import { pluginTypeCheck } from '@rsbuild/plugin-type-check'
 
 export default defineConfig({
-  source: {
-    entry: './src/index.jsx',
-  },
   plugins: [
     pluginQRCode({
       schema(url) {
@@ -15,5 +13,6 @@ export default defineConfig({
       },
     }),
     pluginReactLynx(),
+    pluginTypeCheck(),
   ],
 })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,9 @@ import arrow from './assets/arrow.png'
 import lynxLogo from './assets/lynx-logo.png'
 import reactLynxLogo from './assets/react-logo.png'
 
-export function App(props) {
+export function App(props: {
+  onRender?: () => void
+}) {
   const [alterLogo, setAlterLogo] = useState(false)
 
   useEffect(() => {
